2d8d73443008ec844115f779bde2f5139854cd24
apoc64 <sschwedt@gmail.com>
2018-08-14 14:05:32 -0600

    changes base url

--- main.bundle.js
+++ main.bundle.js
@@ -306,7 +306,7 @@
 
 	'use strict';
 
-	var baseURL = `http://localhost:3000`
+	var baseURL = `https://fathomless-beach-36935.herokuapp.com`
 
 	var foodsAPIFetch = function foodsAPIFetch(id, method, body) {
 	  return fetch(baseURL + '/api/v1/foods/' + id, {
@@ -411,7 +411,7 @@
 
 	var foodsRequests = __webpack_require__(1);
 
-	var baseURL = `http://localhost:3000`
+	var baseURL = `https://fathomless-beach-36935.herokuapp.com`
 
 	var mealsAPIFetch = function mealsAPIFetch(id, method, extension) {
 	  return fetch(baseURL + '/api/v1/meals/' + id + extension, {
